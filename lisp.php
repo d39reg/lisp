656bc8a1da00ff8ca6455d2295c6dd036eed7e9a
Pavel <d39reg@gmail.com>
2019-01-15 13:02:47 +0200

    Update lisp.php

--- lisp.php
+++ lisp.php
@@ -6,6 +6,7 @@
 		private $local = 0;
 		private $l = 0;
 		private $code = '';
+		private $args = [];
 		private function chkWhite($s)
 		{
 			return $s == ' ' || $s == "\t" || $s == "\r" || $s == "\n";
@@ -57,6 +58,19 @@
 					if($args[0] < $args[1]) return true;
 					return false;
 				break;
+				case '>=':
+					if($args[0] >= $args[1]) return true;
+					return false;
+				break;
+				case '<=':
+					if($args[0] <= $args[1]) return true;
+					return false;
+				break;
+				case 'len':
+					$len = strlen($args[0]);$i++;
+					while($i < $l) $len += strlen($args[$i++]);
+					return $len;
+				break;
 				case 'print':
 					while($i < $l) echo $args[$i++];
 				break;
@@ -64,27 +78,20 @@
 					return $args[0];
 				break;
 				case 'eval':
-					
-					for($i = 0; $i < count($args); $i++)
-					{
-						$this->eval($args[$i]);
-					}
+					for($i = 0; $i < count($args); $i++) $this->eval($args[$i]);
 				break;
 				case 'if':
-					if($args[0]) return $args[1];
+					if($args[0]) return $this->eval($args[1],[]);
+				break;
+				case 'loop':
+					for($i = 0; $i < $args[0]; $i++) $this->eval($args[1],[$i]);
 				break;
 				case 'setq':
-					if($args[0][0] != '*')
-					{
-						$this->global[$args[0]] = $args[1];
-					}
-					else $this->local[$args[0]] = $args[1];
+					if($args[0][0] != '*') return $this->global[$args[0]] = $args[1];
+					return $this->local[$args[0]] = $args[1];
 				break;
 				case 'get':
-					if($args[0][0] != '*')
-					{
-						return $this->global[$args[0]];
-					}
+					if($args[0][0] != '*') return $this->global[$args[0]];
 					return $this->local[$args[0]];
 				break;
 				case 'random':
@@ -92,7 +99,9 @@
 				break;
 				
 				default:
-					return call_user_func_array($name, $args);
+					//return call_user_func_array($name, $args);
+					if($name[0] != '*') return $this->eval($this->global[$name], $args);
+					return $this->eval($this->local[$name], $args);
 				break;
 			}
 		}
@@ -129,6 +138,19 @@
 						if(gettype($tmp) == 'string') $type = 2;
 						elseif(gettype($tmp) == 'integer') $type = 1;
 						else $type = 0;
+					}
+					elseif($s == '@')
+					{
+						$s = $this->code[$this->i++];
+						$tmp = '';
+						while($s >= '0' && $s <= '9')
+						{
+							$tmp .= $s;
+							$s = $this->code[$this->i++];
+						}
+						$tmp = $this->args[(int)$tmp];
+						--$this->i;
+						$type = 1;
 					}
 					elseif($s >= '0' && $s <= '9')
 					{
@@ -172,25 +194,29 @@
 			if($s != ')') return;
 			return $this->stdcall($name, $args, $types);
 		}
-		public function eval($code)
+		public function eval($code, $args)
 		{
 			$tmpI = $this->i;
 			$tmpL = $this->l;
 			$tmpC = $this->code;
 			$tmpG = $this->global;
+			$tmpA = $this->args;
 			
 			$this->i = 0;
 			$this->l = strlen($code);
 			
 			$this->local = $this->global;
 			$this->code = $code;
+			$this->args = $args;
+			
+			$ret = 0;
 			
 			while($this->i < strlen($code))
 			{
 				while($this->chkWhite($code[$this->i++]));
 				if($code[$this->i-1] != '(') return;
 				$this->local = $this->global;
-				$this->e();
+				$ret = $this->e();
 				if($code[$this->i-1] != ')') return;
 			}
 			
@@ -198,9 +224,12 @@
 			$this->l      = $tmpL;
 			$this->code   = $tmpC;
 			$this->global = $tmpG;
+			$this->args   = $tmpA;
+			
+			return $ret;
 		}
 	}
 	$l = new lisp();
-	$l->eval('(eval "(print 1)(print 3)")(eval "(print 2)")');
+	$l->eval(file_get_contents('test.lisp'),[]);
 	
 ?>
